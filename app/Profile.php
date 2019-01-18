3ac4bd610f8c6f1ccd01604bc81395dfd196d7b6
Ali Fazel <alifazel@Alis-MBP.lan>
2019-01-18 11:47:50 +0100

    Added User Profile to Dashboard
    Dashboard displays user with their basic profile. No profile returns an error to ask user to create a profile

--- app/Profile.php
+++ app/Profile.php
@@ -12,4 +12,35 @@ class Profile extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function getAge()
+    {
+        // Get DOB and current date and convert to 'datetime' format
+        $dob = date_create_from_format('Y-m-d', $this->dob);
+        $now = date_create_from_format('Y-m-d', date('Y-m-d'));
+        
+        // Compare the user's date of birth with today's date and format to string 'Y'.
+        $age = date_diff($dob, $now)->format('%y');
+
+        return $age;
+    }
+    
+    public function hasParents() {
+            return ($this->father_id || $this->mother_id) ? true : false;
+    }
+
+    public function getParents()
+    {
+        // Try to find parents profiles
+        $father = Profile::find($this->father_id);
+        $mother = Profile::find($this->mother_id);
+
+        // Return 'Name' and 'ID' if exists, otherwise NULL.
+        return ['father_id'     => isset($father) ? $father->id : null ,
+                'father_name'   => isset($father) ? $father->first_name . ' ' . $father->last_name : null,
+                'mother_id'     => isset($mother) ? $mother->id : null,
+                'mother_name'   => isset($mother) ? $mother->first_name . ' ' . $mother->last_name : null,
+            ];
+    }
+    
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
+
+@if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+@endif
+
+
 <div class="container">
+
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+        <div class="col-md-10">
+            <!-- Check if user has profile -->
+            @if (!$profile)
+            <div class="card mb-3">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Create your profile now!</h5>
+                    <p class="card-text">We found that you do not currently have a profile attached to your account. Before going any further, why don't you create one now?</p>
+                    <a href="profile/create" class="btn btn-primary">Create now!</a>
                 </div>
             </div>
+            @else
+            <!-- Display Basic Profile -->
+            <div class="card-group mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3 text-center">
+                                <!-- Profile Avatar -->
+                                <img class="rounded" src="http://i.pravatar.cc/150"/>
+                            </div>
+                            <div class="col-md-6">
+                                <!-- Profile Information -->
+                                <h5 class="card-title">{{ $profile->first_name }} {{ $profile->last_name }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Aged {{ $profile->getAge() }}, Living in {{ $profile->location }}</h6>
+                                <!-- Profile - Parents info -->
+                                @if ($profile->hasParents())
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                {{ $profile->gender == "male" ? "Son of" : "Daughter of" }}
+                                    @if ( $profile->getParents()['father_name'] && $profile->getParents()['mother_name'])
+                                            <a href="{{ $profile->getParents()['father_id'] }}">{{ $profile->getParents()['father_name'] }}</a> and <a href="{{ $profile->getParents()['mother_id'] }}">{{ $profile->getParents()['mother_name'] }}</a>
+                                        @elseif ( $profile->getParents()['father_name'] )
+                                        <a href="{{ $profile->getParents()['father_id'] }}">{{ $profile->getParents()['father_name'] }}</a>
+                                        @elseif ( $profile->getParents()['mother_name'] )
+                                        <a href="{{ $profile->getParents()['mother_id'] }}">{{ $profile->getParents()['mother_name'] }}</a>
+                                        @endif
+                                </h6>
+                                @endif
+                                <!-- Profile Bio -->
+                                <p class="card-text"><span class="font-weight-bold">Biography: </span>{{ $profile->description }}</p>
+                            </div>
+                            <div class="col-md-3 align-self-center">
+                                <button type="button" class="btn btn-outline-secondary btn-sm btn-block" style="white-space: normal">View Profile</button>
+                                <button type="button" class="btn btn-outline-secondary btn-sm btn-block" style="white-space: normal">Edit Profile</button>
+                                <button type="button" class="btn btn-primary btn-sm btn-block" style="white-space: normal">Browse Family Tree</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
         </div>
+    </div>
+
+    <div class="row justify-content-center">
+            <!-- Timeline -->
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-header">
+                        Timeline
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Sidebar -->
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header">
+                        Sidebar
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
     </div>
 </div>
 @endsection
